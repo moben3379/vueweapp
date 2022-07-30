e0654c068b67b3e418a70c6b03fc0c41331fd10e
hfy <2438705723@qq.com>
2022-07-30 16:27:30 +0800

    添加订单在maincontent中的显示

--- src/http/api.js
+++ src/http/api.js
@@ -3,10 +3,12 @@ import * as login from "@/http/modules/login";
 import * as role from "@/http/modules/role"
 import * as menu from "@/http/modules/menu"
 import * as user from "@/http/modules/user";
+import * as orderList from "@/http/modules/orderList";
 
 export default {
     login,
     role,
     menu,
     user,
+    orderList
 }

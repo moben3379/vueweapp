aa7e9d8d65554de689ead7accec63e13dea47712
liuwei <499271211@qq.com>
2022-08-03 10:59:05 +0800

    生命周期优化进入页面展示菜单，修改需要点击刷新显示修改后的菜单，location.reload()加再确定按钮无效，暂时不理

--- src/views/FoodMenu.vue
+++ src/views/FoodMenu.vue
@@ -121,6 +121,7 @@ export default {
       // this.$api.foodmenu.alterMenuInformation("getmenu/alterMenuInformation",JSON.stringify(this.FoodMenu))
       this.getMenuInformation(),
       this.hidebg()
+
     },
 
 

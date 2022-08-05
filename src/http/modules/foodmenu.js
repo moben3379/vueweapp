4e66dc5d1bd053564422d6562677a99e93841088
moben3379 <102411528+moben3379@users.noreply.github.com>
2022-08-05 18:13:31 +0800

    Update foodmenu.js

--- src/http/modules/foodmenu.js
+++ src/http/modules/foodmenu.js
@@ -2,19 +2,19 @@ import * as axios from "@/http/axios";
 
 
 export function findMenuInformation(url,params){
-    return get(url,params);
+    return axios.get(url,params);
 }
 
 export function alterMenuInformation(url,params){
-    return get(url,params);
+    return axios.get(url,params);
 }
 
-export  function  searchMenu (url,params){
-    return post(url,params)
-
+export  function  searchMenu (url,params) {
+    return axios.post(url, params)
+}
 
 export function addMenuInformation(url,params){
-    return get(url,params);
+    return axios.get(url,params);
 }
 
 export function deleteMenu(url,params) {
